e25c007ab15523fab2096636bc9a3c97c0b99a55
James Glass <james.glass14@gmail.com>
2022-04-02 16:51:45 -0500

    a star path finding

--- LD50/Assets/Scripts/HeroController.cs
+++ LD50/Assets/Scripts/HeroController.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class HeroController : MonoBehaviour
 {
+    // A Star Pathfinding
+    public Transform target;
+
+    // public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    // End A Star Pathfinding
+
     private float speed = 1f;
     private float friction = 0.9f;
     private float max_speed = 4f;
@@ -15,21 +29,74 @@ public class HeroController : MonoBehaviour
     public SpriteRenderer sr;
     public Animator animator;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical");
-        direction = direction.normalized;
+        seeker = GetComponent<Seeker>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.1f);
+
     }
 
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
     void FixedUpdate()
     {
+        // direction.x = Input.GetAxisRaw("Horizontal");
+        // direction.y = Input.GetAxisRaw("Vertical");
+        // direction = direction.normalized;
+
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+
         animate();
         movement();
 
         rb.velocity = velocity;
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
     }
+
+    // void FixedUpdate()
+    // {
+    // animate();
+    // movement();
+
+    // rb.velocity = velocity;
+    // }
 
     void movement()
     {
